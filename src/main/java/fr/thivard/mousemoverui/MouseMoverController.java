4a058cc6159efb9e1b7ce95eff41c4374fc491de
Joris69320 <joris.thivard@hotmail.com>
2023-01-30 18:58:13 +0100

    fix

--- src/main/java/fr/thivard/mousemoverui/MouseMoverController.java
+++ src/main/java/fr/thivard/mousemoverui/MouseMoverController.java
@@ -54,7 +54,7 @@ public class MouseMoverController {
                         }
                         refreshTimer(60);
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        System.out.println(e.getMessage());
                         System.out.println(AppManager.app.getThreadName()+" STOPPED");
                     }
                 }
