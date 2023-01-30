337cab7124973559d0eac4d13c5c08b7fe6a8b14
Joris69320 <joris.thivard@hotmail.com>
2023-01-30 20:50:23 +0100

    patch

--- src/main/java/fr/thivard/mousemoverui/Launcher.java
+++ src/main/java/fr/thivard/mousemoverui/Launcher.java
@@ -1,7 +1,10 @@
 package fr.thivard.mousemoverui;
 
+import org.apache.log4j.PropertyConfigurator;
+
 public class Launcher {
     public static void main(String[] args){
+        PropertyConfigurator.configure(Launcher.class.getResourceAsStream("log4j.properties"));
         MouseMoverApplication.main(args);
     }
 }
