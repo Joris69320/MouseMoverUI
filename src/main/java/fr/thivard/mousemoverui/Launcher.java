90472fef00e543accbee555f57ad9b70c159145f
Joris69320 <joris.thivard@hotmail.com>
2023-02-08 14:48:52 +0100

    Code smell

--- src/main/java/fr/thivard/mousemoverui/Launcher.java
+++ src/main/java/fr/thivard/mousemoverui/Launcher.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.core.config.Configurator;
 
 public class Launcher {
 
-    public static final String startLog = """
+    public static final String STARTLOG = """
                             
                             ---------------------------------------------
                                 MouseMoverUI started on version 1.1.3
@@ -14,7 +14,7 @@ public class Launcher {
 
     public static void main(String[] args){
         Configurator.initialize("Config", String.valueOf(Launcher.class.getResource("log4j.properties")));
-        AppManager.app.info(startLog);
+        AppManager.app.info(STARTLOG);
         MouseMoverApplication.main(args);
     }
 }
